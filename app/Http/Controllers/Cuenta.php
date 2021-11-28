b4dc8ca026c149b99394ad5f3e82a4e6130a7e37
feraragon <mferaragonz@hotmail.com>
2021-11-28 11:07:15 -0600

    Base del frontend

---
+++ app/Http/Controllers/Cuenta.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Cuenta extends Controller
+{
+    //
+
+    public function base()
+    {
+
+        return view('base');
+    }
+}
