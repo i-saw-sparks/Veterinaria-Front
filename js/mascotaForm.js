a2b81b883ef91dbaac625477a87d6fe43ad79151
Andres Molina <andresmolinaceti@gmail.com>
2021-11-30 17:49:30 -0600

    mascotas form

---
+++ js/mascotaForm.js
@@ -0,0 +1,43 @@
+let productoController;
+
+(async()=>{
+    let clientes = await get('cliente');
+
+    /**
+     * Nombre:
+     * ID
+     */
+
+    clientes = clientes.map(p => ({nombre: p.nombre, id: p.id}));
+
+    const test = [
+        {
+            id : 'nombre',
+            type : 'text',
+            placeholder: 'Ingrese el nombre de la mascota',
+            name : 'Nombre'
+        },
+        {
+            id : 'fecha_nacimiento',
+            type : 'date',
+            placeholder : '',
+            name : 'Fecha de nacimiento'
+        },
+        {
+            id : 'especie',
+            type : 'text',
+            placeholder : 'Ingrese la especie de la mascota',
+            name : 'Especie'
+        },        
+        {
+            id : 'id_cliente',
+            type : 'select',
+            options: clientes,
+            placeholder : 'Ingrese el cliente dueño de la mascota',
+            name: 'Dueño'
+        }
+    ];
+
+    clienteController = new FormBuilder(test, 'form_content_', 'form', 'mascota');
+    
+})();
