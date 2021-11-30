5a9abf4f43998e14a1174dc7403926602e56c95a
Andres Molina <andresmolinaceti@gmail.com>
2021-11-30 16:53:17 -0600

    Citas form

---
+++ js/citaForm.js
@@ -0,0 +1,58 @@
+let productoController;
+
+(async()=>{
+    let usuarios = await get('usuario');
+    let mascotas = await get('mascota');
+
+    /**
+     * Nombre:
+     * ID
+     */
+
+    usuarios = usuarios.map(p => ({nombre: p.nombre, id: p.id}));
+    mascotas = mascotas.map(p => ({nombre: p.nombre, id: p.id}));
+
+    const test = [
+        {
+            id : 'fecha',
+            type : 'text',
+            placeholder: 'Ingrese la fecha de la cita: AAAA-MM-DD',
+            name : 'Fecha'
+        },
+        {
+            id : 'hora_inicio',
+            type : 'text',
+            placeholder : 'Ingrese la hora de inicio de la cita: HH:MM:SS',
+            name : 'Hora de inicio'
+        },
+        {
+            id : 'hora_fin',
+            type : 'text',
+            placeholder : 'Ingrese la hora de fin de la cita: HH:MM:SS',
+            name : 'Hora de fin'
+        },
+        {
+            id : 'tipo',
+            type : 'text',
+            placeholder: 'Ingrese el tipo de cita',
+            name : 'Tipo de cita'
+        },
+        {
+            id : 'id_usuario',
+            type : 'select',
+            placeholder : 'Ingrese el usuario que atenderá esta cita',
+            name : 'Usuario encargado',
+            options : usuarios
+        },
+        {
+            id : 'id_mascota',
+            type : 'select',
+            placeholder : 'Ingrese la mascota será atendida',
+            name : 'Mascota atendida',
+            options : mascotas
+        }
+    ];
+
+    productoController = new FormBuilder(test, 'form_content_', 'form', 'cita');
+    
+})();
