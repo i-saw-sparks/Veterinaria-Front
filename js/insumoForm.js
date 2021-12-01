b236fc5029d60666dcdec4e301caacd9678d0fd0
Andres Molina <andresmolinaceti@gmail.com>
2021-11-30 18:29:57 -0600

    agregar insumos

---
+++ js/insumoForm.js
@@ -0,0 +1,51 @@
+let productoController;
+
+(async()=>{
+    let proveedores = await get('proveedor');
+
+    proveedores = proveedores.map(p => ({nombre: p.nombre, id: p.id}));
+
+    console.log(proveedores);
+    const test = [
+        {
+            id : 'nombre',
+            type : 'text',
+            placeholder: 'Ingrese el nombre del producto',
+            name : 'Nombre'
+        },
+        {
+            id : 'cantidad',
+            type : 'number',
+            placeholder : 'Ingrese la cantidad de producto existente',
+            name : 'Cantidad'
+        },
+        {
+            id : 'precio',
+            type : 'number',
+            placeholder : 'Ingrese el precio de compra del producto',
+            name : 'Precio de compra'
+        },
+        {
+            id : 'tipo',
+            type : 'text',
+            placeholder: 'Ingrese el tipo de producto',
+            name : 'Tipo de producto'
+        },
+        {
+            id : 'descripcion',
+            type : 'text',
+            placeholder : 'Ingrese la descripción detallada del producto',
+            name: 'Descripcion'
+        },
+        {
+            id : 'id_proveedor',
+            type : 'select',
+            placeholder : 'Ingrese el proveedor que lo surte',
+            name : 'Proveedor',
+            options : proveedores
+        }
+    ];
+
+    insumoController = new FormBuilder(test, 'form_content_', 'form', 'insumo');
+    
+})();
