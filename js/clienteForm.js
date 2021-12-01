52041850628dd4caab9b4470cc1b7418fccdb11a
Andres Molina <andresmolinaceti@gmail.com>
2021-11-30 18:06:07 -0600

    clientes form

---
+++ js/clienteForm.js
@@ -0,0 +1,37 @@
+let productoController;
+
+(async()=>{
+    let clientes = await get('cliente');
+
+    clientes = clientes.map(p => ({nombre: p.nombre, id: p.id}));
+
+    const test = [
+        {
+            id : 'nombre',
+            type : 'text',
+            placeholder: 'Ingrese el nombre del cliente',
+            name : 'Nombre'
+        },
+        {
+            id : 'telefono',
+            type : 'text',
+            placeholder : 'Ingrese el numero de telefono del cliente',
+            name : 'Numero de telefono'
+        },
+        {
+            id : 'direccion',
+            type : 'text',
+            placeholder : 'Ingrese la direccion del cliente',
+            name : 'Direccion'
+        },        
+        {
+            id : 'email',
+            type : 'text',
+            placeholder : 'Ingrese el email del cliente',
+            name: 'Email'
+        }
+    ];
+
+    clienteController = new FormBuilder(test, 'form_content_', 'form', 'cliente');
+    
+})();
